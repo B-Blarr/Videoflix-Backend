689c5502f7553f9b3a05e800cc66e6ccf73dcbe2
Benjamin Blarr <b.blarr@gmx.de>
2026-08-09 13:24:25 +0200

    Enhance registration serializer with password confirmation validation and user creation logic; update User model string representation

--- auth_app/api/serializers.py
+++ auth_app/api/serializers.py
@@ -1,12 +1,14 @@
+from django.contrib.auth import password_validation, get_user_model
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from django.contrib.auth.models import User
+
 from rest_framework import serializers
 
+
+User = get_user_model()
 
 class RegistrationSerializer(serializers.ModelSerializer):
 
     confirmed_password = serializers.CharField(write_only=True)
-    email = serializers.EmailField(required=True)
 
     class Meta:
         model = User
@@ -16,3 +18,39 @@ class RegistrationSerializer(serializers.ModelSerializer):
                 'write_only': True
             }
         }
+
+    def validate_password(self, value):
+
+        password_validation.validate_password(value)
+        return value
+
+    def validate(self, attrs):
+
+        if attrs['password'] != attrs['confirmed_password']:
+            raise serializers.ValidationError(
+                {'confirmed_password': 'Passwords do not match'})
+        return attrs
+
+    def create(self, validated_data):
+
+        pw = validated_data['password']
+        account = User(
+            email=validated_data['email'],
+            username=validated_data['email'],
+            is_active=False
+        )
+        account.set_password(pw)
+        account.save()
+        return account
+
+
+class CookieTokenObtainPairSerializer(TokenObtainPairSerializer):
+
+    def validate(self, attrs):
+ 
+        data = super().validate(attrs)
+        data['user'] = {
+            'id': self.user.id,
+            'username': self.user.email,
+        }
+        return data
