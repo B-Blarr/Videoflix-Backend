82d35fc4d27e67987febfcf3f70ffffb481883d1
Benjamin Blarr <b.blarr@gmx.de>
2026-08-16 21:05:40 +0200

    Add error handling for invalid JWT tokens in CookieJWTAuthentication

--- auth_app/api/authentication.py
+++ auth_app/api/authentication.py
@@ -1,4 +1,5 @@
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.exceptions import InvalidToken
 
 
 class CookieJWTAuthentication(JWTAuthentication):
@@ -9,5 +10,8 @@ class CookieJWTAuthentication(JWTAuthentication):
         raw_token = request.COOKIES.get('access_token')
         if not raw_token:
             return None
-        validated_token = self.get_validated_token(raw_token)
+        try:
+            validated_token = self.get_validated_token(raw_token)
+        except InvalidToken:
+            return None
         return self.get_user(validated_token), validated_token
