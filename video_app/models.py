5595ddbfc4ed3c4e04cbf3cf80419dceaac3a234
Benjamin Blarr <b.blarr@gmx.de>
2026-08-17 11:54:01 +0200

    Define Video model with fields for created_at, title, description, video_file, thumbnail, and category

--- video_app/models.py
+++ video_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Video(models.Model):
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    video_file = models.FileField(upload_to='videos/')
+    thumbnail = models.FileField(upload_to='thumbnails/', blank=True, null=True)
+    category = models.CharField(max_length=200)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
+    
