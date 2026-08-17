c7d520317a136a26941299bdbdbe4564df6f757a
Benjamin Blarr <b.blarr@gmx.de>
2026-08-17 15:18:46 +0200

    Add video app with Video model, views, and API endpoints

---
+++ video_app/api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from .views import VideoListView
+
+
+
+urlpatterns = [
+    path('video/', VideoListView.as_view(), name='video'),
+]
---
+++ video_app/api/views.py
@@ -0,0 +1,10 @@
+from rest_framework import generics
+
+from video_app.models import Video
+from .serializers import VideoSerializer
+
+
+class VideoListView(generics.ListAPIView):
+
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializer
