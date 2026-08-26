25f1e9daaed41e20ad4049e60ff4aea59359d96b
Benjamin Blarr <b.blarr@gmx.de>
2026-08-26 16:19:29 +0200

    Add HLS view tests for video playlist and segment retrieval

--- video_app/tests/tests.py
+++ video_app/tests/tests.py
@@ -1,11 +1,24 @@
 import json
 import subprocess
+import os
+import shutil
+import tempfile
+
 from unittest.mock import patch
-
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.test import override_settings
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework_simplejwt.tokens import RefreshToken
 
+from video_app.models import Video
 from video_app.utils import probe_video, VideoProbeError
 
+
+User = get_user_model()
+TEST_MEDIA = tempfile.mkdtemp()
 
 FFPROBE_JSON = json.dumps({
     'streams': [{'width': 1920, 'height': 1080, 'r_frame_rate': '25/1'}],
@@ -36,4 +49,52 @@ class ProbeVideoTests(TestCase):
         with self.assertRaises(VideoProbeError):
             probe_video('/fake/video.mp4')
 
-            
+
+
+@override_settings(MEDIA_ROOT=TEST_MEDIA)
+class HLSViewTests(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='v@test.de', email='v@test.de',
+            password='SuperSecret123!')
+        self.client.cookies['access_token'] = str(
+            RefreshToken.for_user(self.user).access_token)
+        self.video = Video.objects.create(
+            title='Test', description='Desc', category='Drama',
+            video_file='videos/test.mp4', thumbnail='thumbnails/test.jpg')
+        folder = os.path.join(
+            TEST_MEDIA, 'videos', str(self.video.id), '480p')
+        os.makedirs(folder, exist_ok=True)
+        with open(os.path.join(folder, 'index.m3u8'), 'w') as playlist:
+            playlist.write('#EXTM3U')
+        with open(os.path.join(folder, 'seg000.ts'), 'wb') as segment:
+            segment.write(b'\x47')
+
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree(TEST_MEDIA, ignore_errors=True)
+        super().tearDownClass()
+
+    def test_playlist_returns_m3u8(self):
+        url = reverse('video_playlist', args=[self.video.id, '480p'])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response['Content-Type'], 'application/vnd.apple.mpegurl')
+
+    def test_segment_returns_ts(self):
+        url = reverse(
+            'video_segment', args=[self.video.id, '480p', 'seg000.ts'])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response['Content-Type'], 'video/MP2T')
+
+    def test_unknown_resolution_returns_404(self):
+        url = reverse('video_playlist', args=[self.video.id, '999p'])
+        self.assertEqual(self.client.get(url).status_code, 404)
+
+    def test_video_list_has_absolute_thumbnail_url(self):
+        response = self.client.get(reverse('video_list'))
+        self.assertTrue(
+            response.data[0]['thumbnail_url'].startswith('http://'))
